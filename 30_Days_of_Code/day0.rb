49cf9f60ab909c96abf3c91604d8e52916f3489e
Oswaldo Linhares <oswaldolinhares@gmail.com>
2016-05-07 11:03:34 -0300

    Day 0

---
+++ 30_Days_of_Code/day0.rb
@@ -0,0 +1,11 @@
+# Ler informação do teclado
+inputString = gets.chomp
+
+# Modifica variável para a entrada do HackerRank
+inputString = "Welcome to 30 Days of Code!"
+
+# Imprime a primeira linha
+puts 'Hello, World.'
+
+# Imprime a Segunda Linha
+puts inputString
